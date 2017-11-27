7899cfada93ac39dcc5d60a70ff71286f7de3a95
pivanov05 <33891800+pivanov05@users.noreply.github.com>
2017-11-27 15:00:09 +0200

    Add files via upload

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class QueueElement2
+    {
+        public string value;
+        public QueueElement2 prev, next;
+    }
+    public class Queue2
+    {
+        QueueElement2 head; // = null
+        QueueElement2 tail; // = null
+
+        public void Enqueue(string v)
+        {
+            if (head == null)
+            {
+                head = tail = new QueueElement2()
+                {
+                    value = v
+                };
+            }
+
+            else
+            {
+                tail = new QueueElement2()
+                {
+                    value = v,
+                    prev = tail
+                };
+                tail.prev.next = tail;
+            }
+        }
+
+        public string Dequeue()
+        {
+            if (head == null)
+            {
+                throw new Exception("The queue is empty");
+            }
+
+            var v = head.value;
+            if (head == tail)
+                head = tail = null;
+            else
+            {
+                head = head.next;
+                head.prev = null;
+            }
+            return v;
+
+        }
+
+        public bool Empty
+        {
+            get { return head == null; }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = new Queue2();
+            string name = "dfs";
+            while(name != string.Empty)
+            {
+                name = Console.ReadLine();
+                q.Enqueue(name);
+            }
+
+            while (!q.Empty)
+            {
+                string names = q.Dequeue();
+                Console.WriteLine(names);
+            }
+            Console.ReadLine();
+        }   
+    }
+}
